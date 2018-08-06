2b61165bfcdaa5c54ce4ee69e4a3fd6ff9565d86
Kwok-Shing Chan <kwocha@mentat003.dccn.nl>
2018-08-06 18:00:23 +0200

    update script for exvivo fitting

---
+++ utils/ComputeMWF.m
@@ -0,0 +1,21 @@
+%% function mwf = ComputeMWF(fitRes)
+%
+% Input
+% --------------
+% fitRes        : struct array from mwi process
+%
+% Output
+% --------------
+% mwf           : myelin water fraction
+%
+% Kwok-shing Chan @ DCCN
+% k.chan@donders.ru.nl
+% Date created: 3 August 2018
+% Date last modified:
+%
+%
+function mwf = ComputeMWF(fitRes)
+
+mwf = (fitRes.estimates(:,:,:,1)) ./ sum(fitRes.estimates(:,:,:,1:3),4);
+
+end
